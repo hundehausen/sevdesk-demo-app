e4c069bb609ac2479252213c7926aee65cbcfd3f
hundehausen <mail@grischadaum.de>
2020-11-01 17:49:58 +0100

    timespan for chart

--- src/views/BitcoinDiagramm.js
+++ src/views/BitcoinDiagramm.js
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import { getCharts } from "../libs/api";
 import Header from "../components/Header";
 import Container from "@material-ui/core/Container";
+import Select from "@material-ui/core/Select";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
 import {
   ResponsiveContainer,
   LineChart,
@@ -13,22 +17,23 @@ import {
   Tooltip,
   Legend,
 } from "recharts";
+import { Typography } from "@material-ui/core";
 
 function Chart({ data }) {
   return (
     <Container>
-      <Header title="Bitcoin Charts" />
       <ResponsiveContainer height={400} width="90%">
+        <Typography variant="h3">{data.name}</Typography>
         <LineChart
-          data={data}
+          data={data.values}
           margin={{ top: 15, right: 15, left: 50, bottom: 15 }}
         >
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="">
-            <Label value="days" offset={0} position="bottom" />
+            <Label value="Tage" offset={0} position="bottom" />
           </XAxis>
           <YAxis>
-            <Label value="USD" offset={0} position="left" />
+            <Label value={data.unit} offset={0} position="left" />
           </YAxis>
           <Tooltip />
           <Legend />
@@ -47,10 +52,8 @@ function BitcoinDiagramm() {
 
   // fetch all charts data from api
   useEffect(() => {
-    if (!charts) {
-      getCharts(chartName, timespan, start).then(setCharts);
-    }
-  }, [charts, setCharts, chartName, timespan, start]);
+    getCharts(chartName, timespan, start).then(setCharts);
+  }, [setCharts, chartName, timespan, start]);
 
   useEffect(() => {
     if (charts) {
@@ -58,7 +61,32 @@ function BitcoinDiagramm() {
     }
   }, [charts]);
 
-  return <div>{charts ? <Chart data={charts.values} /> : null}</div>;
+  function handleTimeRangeChange(event) {
+    setTimespan(event.target.value);
+    console.log("event.target.value", event.target.value);
+  }
+
+  return (
+    <div>
+      <Header title="Bitcoin Charts" />
+      <FormControl>
+        <InputLabel id="zeitraum-label">Zeitraum</InputLabel>
+        <Select
+          labelId="zeitraum-label"
+          id="zeitraum"
+          value={timespan}
+          onChange={handleTimeRangeChange}
+        >
+          <MenuItem value={"1weeks"}>1 Woche</MenuItem>
+          <MenuItem value={"1months"}>1 Monat</MenuItem>
+          <MenuItem value={"3months"}>3 Monate</MenuItem>
+          <MenuItem value={"6months"}>6 Monate</MenuItem>
+          <MenuItem value={"1year"}>1 Jahr</MenuItem>
+        </Select>
+      </FormControl>
+      {charts ? <Chart data={charts} /> : null}
+    </div>
+  );
 }
 
 export default BitcoinDiagramm;
